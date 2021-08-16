f67fbc9b6820ebd98b35fae704c317de2fa532c8
Vivek Subraveti <viveksubraveti@gmail.com>
2021-08-16 18:59:41 +0200

    Initial commit
    First commit to facial detection

---
+++ FaceLandmarkDetection/LandmarkDetection.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+import dlib
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+detector = dlib.get_frontal_face_detector()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
