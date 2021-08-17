511195499c0f745089fd419bd27c56140b4fab02
Vivek Subraveti <viveksubraveti@gmail.com>
2021-08-17 08:39:18 +0200

    Facial landmark detection
    a complete facial landmark detection applicaiton

--- FaceLandmarkDetection/LandmarkDetection.py
+++ FaceLandmarkDetection/LandmarkDetection.py
@@ -1,12 +1,10 @@
-import numpy as np
 import cv2
 import dlib
 
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
     # Capture frame-by-frame
@@ -14,12 +12,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
 
+    # read the face in live and find the coordinates
     for face in faces:
         x1 = face.left()
         y1 = face.top()
         x2 = face.right()
         y2 = face.bottom()
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        landmarks = predictor(gray, face)
+
+        # using the face landmarks model find all 68 points
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
@@ -29,6 +36,7 @@ while True:
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
+
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
